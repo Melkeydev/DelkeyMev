dba1505ecc9d28687d3fe9aa7f6caf1e099418c8
Logan Purcell <loganpurcell20@protonmail.com>
2020-07-16 00:32:20 -0400

    A real useful refactor that will allow us to pass arguments to commands in the future and do clear and kick/ban commands

--- bot.py
+++ bot.py
@@ -1,32 +1,40 @@
 import discord
 from discord.ext import commands
 
-client = discord.Client()
+bot = commands.bot(command_prefix = '!')
 token = open("token.txt", "r").read()
 
-@client.event
+@bot.event
 async def on_ready():
   print(f"We are live as {client.user}")
 
-@client.event
-async def on_message(message):
-  if "twitch()" == message.content.lower() and 'commands' == str(message.channel):
-    await message.channel.send("Go Follow Melkeydev over at https://www.twitch.tv/melkeydev")
+@bot.command()
+async def twitch(ctx):
+  ctx.send("Go Follow Melkeydev over at https://www.twitch.tv/melkeydev")
 
-  if "test()" == message.content.lower() and 'commands' == str(message.channel):
-    await message.channel.send("Test worked you didnt break it yet")
+@bot.command()
+async def test(ctx):
+  ctx.send("Test worked you didnt break it yet")
 
-  if "schedule()" == message.content.lower() and 'commands' == str(message.channel):
-    await message.channel.send("Melkey streams start on Mondays, Wednesdays, and Fridays at 9PM EST")
+@bot.command()
+async def schedule(ctx):
+  ctx.send("Melkey streams start on Mondays, Wednesdays, and Fridays at 9PM EST")
 
-  if "project()" == message.content.lower() and 'commands' == str(message.channel):
-    await message.channel.send("Melkey is working on a NBA app written in react to search, and compare player stats! You can find it over at https://github.com/Amokstakov/NewNBAApp")
+@bot.command()
+async def project(ctx):
+  ctx.send("Melkey is working on a NBA app written in react to search, and compare player stats!")
 
-  if "pow()" == message.content.lower() and 'commands' == str(message.channel):
-    await message.channel.send("The pow is a sacred technique practice by the ancient tribes of Konoha.")
+@bot.command()
+async def pow(ctx):
+  ctx.send("The pow is a sacred technique practice by the ancient tribes of Konoha.")
 
-  if "crash()" == message.content.lower() and 'commands' == str(message.channel):
-    await message.channel.send("Nice try I am impossible to crash")
+@bot.command()
+async def crash(ctx):
+  ctx.send("Nice try I am impossible to crash")
 
-client.run(token)
+@bot.command()
+async def dot(ctx):
+  ctx.send("Check out my dotfiles at https://github.com/Amokstakov/NvimConfig")
+
+bot.run(token)
 
